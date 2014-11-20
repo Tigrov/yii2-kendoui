3716fcdc5a00c02f25da43ecfbdc14857fac431e
Supply.Parts <www.supply.parts@gmail.com>
2014-11-20 14:17:51 +1000

    +DataSource::schema() +DataSource::model()

--- DataSource.php
+++ DataSource.php
@@ -210,7 +210,31 @@ class DataSource extends Object
     }
 
     /**
-     * Settings for model object
+     * Static method: Settings for schema object
+     *
+     * @param \yii\db\ActiveRecord|mixed $model
+     * @param array $config ['data' => 'data', 'total' => 'total', 'errors' => 'errors', 'groups' => 'groups', 'aggregates' => 'aggregates',
+     *      'attributeNames' => [...], 'exceptAttributes' => [...], 'extraFields' => [...], 'extendMode' => true|false]
+     * @param array $args the constructor parameters for model
+     * @return array
+     */
+    public static function schema($model, array $config = [], array $args = [])
+    {
+        $schema = [];
+        foreach (['data', 'total', 'errors', 'groups', 'aggregates'] as $param) {
+            $schema[$param] = isset($config[$param]) ? $config[$param] : $param;
+            unset($config[$param]);
+        }
+
+        if ($model) {
+            $schema['model'] = static::model($model, $config, $args);
+        }
+
+        return $schema;
+    }
+
+    /**
+     * Settings for schema model
      *
      * @return array
      */
@@ -218,59 +242,81 @@ class DataSource extends Object
     {
         $actionInstance = $this->getActionInstance();
         $modelInstance = $actionInstance->getModelInstance();
-        $columns = $modelInstance->getTableSchema()->columns;
-        $editableAttributes = $modelInstance->activeAttributes();
 
-        $attributes = $actionInstance->getAttributes(true);
-        $extraFields = $actionInstance->getExtraFields();
-        $extendMode = $actionInstance->getExtendMode();
-        $fields = $extendMode ? $modelInstance->fields() : [];
+        return static::model($modelInstance, [
+            'attributeNames' => $actionInstance->getAttributes(true),
+            'extraFields' => $actionInstance->getExtraFields(),
+            'extendMode' => $actionInstance->getExtendMode(),
+        ]);
+    }
 
-        $model = [];
-        $keys = $modelInstance::primaryKey();
+    /**
+     * Static method: setting for schema model
+     *
+     * @param \yii\db\ActiveRecord|mixed $model
+     * @param array $config ['attributeNames' => [...], 'exceptAttributes' => [...], 'extraFields' => [...], 'extendMode' => true|false]
+     * @param array $args the constructor parameters for model
+     * @return array
+     */
+    public static function model($model, array $config = [], array $args = [])
+    {
+        $model = is_object($model) ? $model : \Yii::createObject($model, $args);
+
+        $columns = $model->getTableSchema()->columns;
+        $editableAttributes = $model->activeAttributes();
+
+        $attributeNames = isset($config['attributeNames']) ? $config['attributeNames'] : $model->attributes();
+        $exceptAttributes = isset($config['exceptAttributes']) ? $config['exceptAttributes'] : [];
+        $attributes = array_diff($attributeNames, $exceptAttributes);
+        $extraFields = isset($config['extraFields']) ? $config['extraFields'] : [];
+        $extendMode = isset($config['extendMode']) || $extraFields;
+        $fields = $extendMode ? $model->fields() : [];
+
+        $result = [];
+        $keys = $model::primaryKey();
         if (count($keys) === 1) {
             $pk = $keys[0];
-            $model['id'] = $pk;
+            $result['id'] = $pk;
             if (!in_array($pk, $attributes)) {
                 $attributes[] = $pk;
             }
         } else {
             $pk = implode('__', $keys);
-            $model['id'] = $pk;
+            $result['id'] = $pk;
             if (!in_array($pk, $extraFields)) {
                 $extraFields[] = $pk;
             }
         }
 
-        $model['fields'] = [];
+        $result['fields'] = [];
         foreach ($attributes as $attr) {
             $field = $extendMode && isset($fields[$attr]) ? $fields[$attr] : $attr;
             $column = $columns[$attr];
 
-            $model['fields'][$field] = [];
+            $result['fields'][$field] = [];
             if (($type = static::convertType($column->type)) !== 'string') {
-                $model['fields'][$field]['type'] = $type;
+                $result['fields'][$field]['type'] = $type;
             }
             if (!$column->allowNull && !$column->autoIncrement) {
-                $model['fields'][$field]['nullable'] = false;
-                $model['fields'][$field]['validation']['required'] = true;
+                $result['fields'][$field]['nullable'] = false;
+                $result['fields'][$field]['validation']['required'] = true;
             }
             if ($column->defaultValue) {
-                $model['fields'][$field]['defaultValue'] = $column->defaultValue;
+                $result['fields'][$field]['defaultValue'] = $column->defaultValue;
             }
             if ($column->unsigned) {
-                $model['fields'][$field]['validation']['min'] = 0;
+                $result['fields'][$field]['validation']['min'] = 0;
             }
             if (!in_array($attr, $editableAttributes)) {
-                $model['fields'][$field]['editable'] = false;
+                $result['fields'][$field]['editable'] = false;
             }
         }
 
         foreach ($extraFields as $field) {
-            $model['fields'][$field] = [];
+            $result['fields'][$field] = [];
         }
 
-        return $model;
+        return $result;
     }
 
     /**
