6ba902cf5ad64a7e7bd2b68b13bfa0c014ba4c07
Supply.Parts <www.supply.parts@gmail.com>
2016-02-22 00:16:28 +0300

    add Kendo::escape()

--- helpers/Kendo.php
+++ helpers/Kendo.php
@@ -18,4 +18,18 @@ class Kendo {
 
         return json_encode($names);
     }
+
+    public static function escape($value, $count = 1)
+    {
+        if (is_array($value)) {
+            return array_map([__CLASS__, 'escapeStr'], $value, array_fill(0, count($value) - 1, $count));
+        }
+
+        return static::escapeStr($value, $count);
+    }
+
+    public static function escapeStr($value, $count = 1)
+    {
+        return str_replace('#', str_repeat('\\', $count) . '#', $value);
+    }
 } 
