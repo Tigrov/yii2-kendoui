4680ec8d3294afee0bdd01c18b411728ec0d95e1
Tigrov <rrr-r@ya.ru>
2018-02-14 20:22:47 +0300

    change schema to https

--- src/assets/KendoAsset.php
+++ src/assets/KendoAsset.php
@@ -5,12 +5,12 @@ class KendoAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = null;
     public $css = [
-        'http://kendo.cdn.telerik.com/2018.1.117/styles/kendo.common-bootstrap.min.css',
-        'http://kendo.cdn.telerik.com/2018.1.117/styles/kendo.bootstrap.min.css',
+        'https://kendo.cdn.telerik.com/2018.1.117/styles/kendo.common-bootstrap.min.css',
+        'https://kendo.cdn.telerik.com/2018.1.117/styles/kendo.bootstrap.min.css',
     ];
     public $js = [
-        'http://kendo.cdn.telerik.com/2018.1.117/js/kendo.all.min.js',
-        //'http://kendo.cdn.telerik.com/2018.1.117/js/kendo.ui.core.min.js',
+        'https://kendo.cdn.telerik.com/2018.1.117/js/kendo.all.min.js',
+        //'https://kendo.cdn.telerik.com/2018.1.117/js/kendo.ui.core.min.js',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
