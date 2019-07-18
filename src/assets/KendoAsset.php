0779c7cdc2ee3e6808ced25d16bfe43582d85c69
Tigrov <rrr-r@ya.ru>
2019-07-18 15:24:37 +0300

    update Kendo Assets version

--- src/assets/KendoAsset.php
+++ src/assets/KendoAsset.php
@@ -15,12 +15,12 @@ class KendoAsset extends \yii\web\AssetBundle
 {
     public $sourcePath = null;
     public $css = [
-        'https://kendo.cdn.telerik.com/2018.1.117/styles/kendo.common-bootstrap.min.css',
-        'https://kendo.cdn.telerik.com/2018.1.117/styles/kendo.bootstrap.min.css',
+        'https://kendo.cdn.telerik.com/2019.2.619/styles/kendo.common-bootstrap.min.css',
+        'https://kendo.cdn.telerik.com/2019.2.619/styles/kendo.bootstrap.min.css',
     ];
     public $js = [
-        'https://kendo.cdn.telerik.com/2018.1.117/js/kendo.all.min.js',
-        //'https://kendo.cdn.telerik.com/2018.1.117/js/kendo.ui.core.min.js',
+        'https://kendo.cdn.telerik.com/2019.2.619/js/kendo.all.min.js',
+        //'https://kendo.cdn.telerik.com/2019.2.619/js/kendo.ui.core.min.js',
     ];
     public $depends = [
         'yii\web\JqueryAsset',
